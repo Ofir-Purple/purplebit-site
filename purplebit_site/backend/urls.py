9c33984a32b95caa679462540b2b4d47cd8718d8
Ofir Ovadia <ofir@purplebit.com>
2014-03-04 03:18:22 +0200

    Removed an old import from DRF

--- purplebit_site/backend/urls.py
+++ purplebit_site/backend/urls.py
@@ -1,5 +1,4 @@
 from django.conf.urls import patterns, url, include
-from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 from django.contrib import admin
@@ -8,6 +7,6 @@ admin.autodiscover()
 urlpatterns = patterns('backend.views',)
 
 # JSON API
-urlpatterns += format_suffix_patterns(patterns('',
+urlpatterns += patterns('',
 	url(r'contact/', views.contact, name="contact"),
-))
+)
