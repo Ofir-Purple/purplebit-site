31488b8ca2d56f43da6023bc16794f19064404cb
Vladimir Zeifman <vladimir@purplebit.com>
2014-03-09 12:04:39 +0200

    Added setDefaults and tryPrisine functions to reset the form to it's default state; Added skeleton code for http post;

--- purplebit_site/main_site/static/main_site/js/angular/contactCtrl.js
+++ purplebit_site/main_site/static/main_site/js/angular/contactCtrl.js
@@ -1,11 +1,16 @@
 
-purplebit.controller('contactCtrl', function($scope) {
+purplebit.controller('contactCtrl', function($scope, $http) {
 
 	$scope.contact = {};
+	
+	// set default form values
+	var setDefaults = function() {
+		$scope.contact.name = undefined;
+		$scope.contact.email = undefined;
+		$scope.contact.message = undefined;
+	}
 
-	$scope.contact.name = undefined;
-	$scope.contact.email = undefined;
-	$scope.contact.message = undefined;
+	setDefaults();
 
 	// when input focus lost - try setting form pristine again
 	$scope.tryPristine = function() {
@@ -19,6 +24,23 @@ purplebit.controller('contactCtrl', function($scope) {
 	// submit function
 	$scope.submit = function() {
 
+		$http.post('/submit', 'carousel')
+
+		.success(function(data, status, headers, config) {
+			// on success
+			console.log('success!')
+		})
+
+		.error(function(data, status, headers, config) {
+			// on error
+			console.log('error happened');
+		});
+
+		// clear form after submit
+		setDefaults();
+
+		// set form pristine
+		$scope.tryPristine();
 	}
 
 
