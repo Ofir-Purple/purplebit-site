c6934cfe2af8d4a893153f4feb334e18366173ee
Vladimir Zeifman <vladimir@purplebit.com>
2014-03-09 10:08:49 +0200

    Code cleanup

--- purplebit_site/main_site/static/main_site/js/angular/purplebit.js
+++ purplebit_site/main_site/static/main_site/js/angular/purplebit.js
@@ -1,16 +1,19 @@
 
 purplebit = angular.module('purplebit', ['ngSanitize']);
 
-
+// change angular default {{ to {$
 purplebit.config(function($interpolateProvider) {
 	$interpolateProvider.startSymbol('{$');
     $interpolateProvider.endSymbol('$}');
 })
 
 
+//purplebit.constant('STATIC_URL', "{% static %}" );
+
+
 // configuration variables
 purplebit.service('configSrv', function() {
 
 	// django static files prefix
-	this.staticUrl = '/static/';
+	this.staticUrl = '';
 })
