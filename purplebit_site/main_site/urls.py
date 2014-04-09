be9cd53acae0750ab5dc2c463bc391833ed0067e
Edan Maor <edan@purplebit.com>
2014-04-09 15:21:20 +0300

    Mostly copy on the main site and on Edan's Pycon 2014 page. Created a Pycon 2014 page for Ofir.

--- purplebit_site/main_site/urls.py
+++ purplebit_site/main_site/urls.py
@@ -3,5 +3,6 @@ from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
     url(r'$^', TemplateView.as_view(template_name='main_site/index.html')),
-    url(r'^p2014e/$', TemplateView.as_view(template_name='main_site/p2014e.html')),
+    url(r'^p4e/$', TemplateView.as_view(template_name='main_site/p4e.html')),
+    url(r'^p4o/$', TemplateView.as_view(template_name='main_site/p4o.html')),
 )
